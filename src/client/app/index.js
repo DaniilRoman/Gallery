a6e0c5f7cac1ebea2e84e2ea45166771bc5f5a1c
Daniil Roman <danroman17397@gmail.com>
2018-03-03 14:49:11 +0300

    test: key and ul-li using

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,112 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
 
-// function UserGreeting(props) {
-//   return <h1>Welcome back!</h1>;
-// }
-
-// function GuestGreeting(props) {
-//   return <h1>Please sign up.</h1>;
-// }
-
-// function Greeting(props) {
-//   const isLoggedIn = props.isLoggedIn;
-//   if (isLoggedIn) {
-//     return <UserGreeting />;
-//   }
-//   return <GuestGreeting />;
-// }
-
-// function LoginButton(props) {
-//   return (
-//     <button onClick={props.onClick}>
-//       Login
-//     </button>
-//   );
-// }
-
-// function LogoutButton(props) {
-//   return (
-//     <button onClick={props.onClick}>
-//       Logout
-//     </button>
-//   );
-// }
-
-// class LoginControl extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleLoginClick = this.handleLoginClick.bind(this);
-//     this.handleLogoutClick = this.handleLogoutClick.bind(this);
-//     this.state = { isLoggedIn: false };
-//   }
-
-//   handleLoginClick() {
-//     this.setState({ isLoggedIn: true });
-//   }
-
-//   handleLogoutClick() {
-//     this.setState({ isLoggedIn: false });
-//   }
-
-//   render() {
-//     const isLoggedIn = this.state.isLoggedIn;
-
-//     let button = null;
-//     if (isLoggedIn) {
-//       button = <LogoutButton onClick={this.handleLogoutClick} />;
-//     } else {
-//       button = <LoginButton onClick={this.handleLoginClick} />;
-//     }
-
-//     return (
-//       <div>
-//         <Greeting isLoggedIn={isLoggedIn} />
-//         {button}
-//       </div>
-//     );
-//   }
-// }
-
-function WarningBanner(props) {
-  if (!props.warn) {
-    return null;
-  }
-
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <ListItem key={number.toString()} value={number}/>
+  );
   return (
-    <div className="warning">
-      Warning!
-    </div>
+    <ul>{listItems}</ul>
   );
 }
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { showWarning: true };
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
 
-  handleToggleClick() {
-    this.setState(prevState => ({
-      showWarning: !prevState.showWarning
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <WarningBanner warn={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? 'Hide' : 'Show'}
-        </button>
-      </div>
-    );
-  }
-}
+const numbers = [1, 2, 3, 4, 5];
 
 ReactDOM.render(
-  <Page />,
+  <NumberList numbers={numbers} />,
   document.getElementById('app')
 );
