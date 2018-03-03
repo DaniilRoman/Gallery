a905c039acdc7aed2f612e49d9d2a94abc12727a
Daniil Roman <danroman17397@gmail.com>
2018-03-03 12:38:26 +0300

    test: toggle ON:OFF button

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -53,40 +53,31 @@ import ReactDOM from 'react-dom';
 //   }
 
 
-class Clock extends React.Component{
-  constructor(props){
+class Toggle extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = { isToggleOn: true };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount(){
-      this.timeID = setInterval(()=>this.tick(),1000);
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
   }
 
-  componentWillUnmount(){
-    clearInterval(this.timerID);
-  }
-
-  tick(){
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render(){
-    return(
-      <div>
-        <h1>Hello world!</h1>
-        <FormattedDate date={this.state.date}/>
-      </div>
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
     );
   }
 }
 
-function FormattedDate(props){
-  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
-}
-
-
-
-ReactDOM.render(<Clock/>, document.getElementById('app'));
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('app')
+);
