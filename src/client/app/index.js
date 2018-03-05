916c598226f94a16d09b5b690b4772bfa0695519
Daniil Roman <daro1017@netcracker.com>
2018-03-05 17:18:17 +0300

    test: ToDo example

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,59 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-function FancyBorder(props) {
-  return (
-    <div className={'FancyBorder FancyBorder-' + props.color}>
-      {props.children}
-    </div>
-  );
-}
+// function FancyBorder(props) {
+//   return (
+//     <div className={'FancyBorder FancyBorder-' + props.color}>
+//       {props.children}
+//     </div>
+//   );
+// }
 
-function Dialog(props) {
-  return (
-    <FancyBorder color="blue">
-      <h1 className="Dialog-title">
-        {props.title}
-      </h1>
-      <p className="Dialog-message">
-        {props.message}
-      </p>
-      {props.children}
-    </FancyBorder>
-  );
-}
+// function Dialog(props) {
+//   return (
+//     <FancyBorder color="blue">
+//       <h1 className="Dialog-title">
+//         {props.title}
+//       </h1>
+//       <p className="Dialog-message">
+//         {props.message}
+//       </p>
+//       {props.children}
+//     </FancyBorder>
+//   );
+// }
 
-class SignUpDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSignUp = this.handleSignUp.bind(this);
-    this.state = { login: '' };
-  }
+// class SignUpDialog extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSignUp = this.handleSignUp.bind(this);
+//     this.state = { login: '' };
+//   }
 
-  render() {
-    return (
-      <Dialog title="Mars Exploration Program"
-        message="How should we refer to you?">
-        <input value={this.state.login}
-          onChange={this.handleChange} />
-        <button onClick={this.handleSignUp}>
-          Sign Me Up!
-        </button>
-      </Dialog>
-    );
-  }
+//   render() {
+//     return (
+//       <Dialog title="Mars Exploration Program"
+//         message="How should we refer to you?">
+//         <input value={this.state.login}
+//           onChange={this.handleChange} />
+//         <button onClick={this.handleSignUp}>
+//           Sign Me Up!
+//         </button>
+//       </Dialog>
+//     );
+//   }
 
-  handleChange(e) {
-    this.setState({ login: e.target.value });
-  }
+//   handleChange(e) {
+//     this.setState({ login: e.target.value });
+//   }
 
-  handleSignUp() {
-    alert(`Welcome aboard, ${this.state.login}!`);
-  }
-}
+//   handleSignUp() {
+//     alert(`Welcome aboard, ${this.state.login}!`);
+//   }
+// }
 
-ReactDOM.render(
-  <SignUpDialog />,
+// ReactDOM.render(
+//   <SignUpDialog />,
+//   document.getElementById('app')
+// );
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+ 
+let store = createStore(todoApp)
+ 
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
-);
+)
