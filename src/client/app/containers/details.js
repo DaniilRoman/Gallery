0fd2b09e7b76ee68d76da07ceb303243682211f3
Daniil Roman <daro1017@netcracker.com>
2018-03-13 19:33:16 +0300

    test: test

--- src/client/app/containers/details.js
+++ src/client/app/containers/details.js
@@ -6,23 +6,38 @@ import Behance from 'behance-api';
 class Details extends Component {
     constructor(props) {
         super(props);
+        this.result = {name:"",img:"",url:""}
+        this.getData = this.getData.bind(this);
         this.Be = new Behance('e1A607WbYauktG2el5XT2dbZriXROx4T');
     }
+
+    getData(th){
+        this.Be.projects({}, function (err, res, data) {
+            if (err) throw err;
+            res = JSON.parse(res.body).projects[3];
+            th.result.name = res.name;
+            th.result.img = res.covers.original;
+            th.result.url = res.url;
+            //let result = data[0]["colors"][0]["b"];
+            //return result
+        })
+    }
+
     render() {
         if (!this.props.car) {
             return (<p>Выберите автомобиль...</p>);
         }
         return (
             <div>
-                {this.Be.projects({ q: 'motorcycle'}, function (err, res, data) {
-                    if (err) throw err;
-                    return data;//console.dir(data);
-                })}
-                <h2>{this.props.car.name}</h2>
+                <p>{this.getData(this)}</p>
+                <h2>{this.result.name}</h2>
+                <img src={this.result.img} /><br />
+                <p>{this.result.url}</p>
+                {/* <h2>{this.props.car.name}</h2>
                 <img src={this.props.car.img} /><br />
                 <p>{this.props.car.desc}</p>
                 <h2>Скорость: {this.props.car.speed},
-                вес: {this.props.car.weight}</h2>
+                вес: {this.props.car.weight}</h2> */}
             </div>
         );
     }
